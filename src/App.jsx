43cb9baada0be00bea4707e6cc0aaab192c5cbbf
KayyuruTharani <kayyurutharani@gmail.com>
2024-07-09 15:00:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import HeaderComponent from './components/HeaderComponent'
+
+const App = ()=>{
+  return(
+    <>
+    <HeaderComponent/>
+    </>
+  )
+}
+   
+
+export default App
