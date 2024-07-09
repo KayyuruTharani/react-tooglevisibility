d3715794743d942f87c6c3bafc52653f4bfc810d
KayyuruTharani <kayyurutharani@gmail.com>
2024-07-09 15:13:04 +0530

    first commit

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -12,8 +12,9 @@ const HeaderComponent = () => {
   return (
     <div className=" min-vw-100 position-relative min-vh-100 d-flex justify-content-center align-items-center animate-bgChange">
       <div className="card p-4 shadow-lg text-center">
-        <div className=" justify-content-center align-items-center"/>
+        <div className=" justify-content-center align-items-center">
         <EyeIcon/>
+        </div>
         <h1 className="text-2xl font-bold mb-3"> Name: Kayyuru Tharani</h1>
         <p className="text-gray-600 mb-3">Registration No: 212221040080</p>
         <button
